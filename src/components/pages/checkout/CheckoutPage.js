03a9f4525de8613ed165f37d7c6e6d18cebfdc57
Rubens Cordeiro <rubensccordeiro@outlook.com>
2022-11-30 10:04:03 -0300

    Small fix

--- src/components/pages/checkout/CheckoutPage.js
+++ src/components/pages/checkout/CheckoutPage.js
@@ -10,7 +10,7 @@ const CheckoutPage = () => {
 
   useEffect(() => {
     setCartOpen(false);
-  }, []);
+  });
 
   const itemList = cartItems.map((cartItem) => (
     <CheckoutItem key={cartItem.name} checkoutItem={cartItem} />
