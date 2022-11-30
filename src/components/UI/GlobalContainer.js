ea2337c1fa65d75b02e596a510705d3a409a3e4b
Rubens Cordeiro <rubensccordeiro@outlook.com>
2022-11-30 13:11:22 -0300

    a couple of fixes

--- src/components/UI/GlobalContainer.js
+++ src/components/UI/GlobalContainer.js
@@ -1,5 +1,9 @@
 const GlobalContainer = ({ children }) => {
-  return <div className="container m-auto">{children}</div>;
+  return (
+    <div className="max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] mx-auto px-8">
+      {children}
+    </div>
+  );
 };
 
 export default GlobalContainer;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,7 +10,16 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -75,4 +84,30 @@ export const signOutUser = async () => {
 export const onAuthStateChangedListener = (callback) => {
   if (callback === null) return;
   onAuthStateChanged(auth, callback);
+};
+
+export const addColletionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey); // The keys are the categories
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach(async (object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, snapshot) => {
+    const { title, items } = snapshot.data();
+    acc[title.toLowerCase()] = items; // { hats: [items_from_hats]}
+    return acc;
+  }, {});
+
+  return categoryMap;
 };
